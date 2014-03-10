35779de790179a4f1acb1b7d086dbc21b9f9c689
fabiogalantemans <fabiogalantemans@gmail.com>
2014-03-10 12:00:01 -0300

    Quiron

---
+++ Quiron.LojaVirtual.Dominio/Repositorio/ProdutosRepositorio.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using Quiron.LojaVirtual.Dominio.Entidades;
+
+namespace Quiron.LojaVirtual.Dominio.Repositorio
+{
+    public class ProdutosRepositorio
+    {
+        private readonly EfDbContext _context = new EfDbContext();
+        public IEnumerable<Produto> Produtos
+        {
+            get { return _context.Produtos; }
+        }
+    }
+}
---
+++ Quiron.LojaVirtual.Web/Controllers/ProdutoController.cs
@@ -0,0 +1,17 @@
+﻿using System.Web.Mvc;
+using Quiron.LojaVirtual.Dominio.Repositorio;
+
+namespace Quiron.LojaVirtual.Web.Controllers
+{
+    public class ProdutoController : Controller
+    {
+        private ProdutosRepositorio _repositorio;
+      
+
+        public ActionResult ListaProdutos()
+        {
+            _repositorio = new ProdutosRepositorio();
+            return View(_repositorio.Produtos);
+        }
+	}
+}
